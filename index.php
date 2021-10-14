48a31872aafae39ee1998def89663058d6269cd3
Michele <micheleierniero@gmail.com>
2021-10-14 17:28:40 +0200

    indentazione

--- index.php
+++ index.php
@@ -32,13 +32,11 @@ $FAQs = [
         "domanda" => "Perché il mio account è associato a un paese?",
         'risposta' => [
             "uno" => "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-
             <ol class='ol' type='a'>
                 <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
                 <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
-            </ol>",
-                
-        "due" => "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+            </ol>", 
+            "due" => "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
         ],
     ],
     [
